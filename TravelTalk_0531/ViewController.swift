4ab977b3579928ff092b7cdf7f61089345651bd3
choiminkyung <choialsrud2002@gmail.com>
2024-06-01 00:59:56 +0900

    Initial Commit

---
+++ TravelTalk_0531/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk_0531
+//
+//  Created by 최민경 on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
